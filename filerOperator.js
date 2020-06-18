e6cc05e4de35cdbe825566161c6e2de4a9030002
Deepak Pookkote <deepak.pookkote@wallstreetenglish.com>
2020-06-18 15:37:27 +0530

    code updated

---
+++ filerOperator.js
@@ -0,0 +1,18 @@
+// var observable = new observable.interval(1000);
+
+// observable
+// filter(function(value) {
+//     return value % 2 === 0;
+// })
+// .subscribe({
+//     next: function(value) {
+//         console.log(value)
+//     },
+//     error: function(error) {
+//         console.log(error, 'Error')
+//     }
+// })
+
+// setTimeout(() => {
+//     observable.unsubscribe();
+// },300);
